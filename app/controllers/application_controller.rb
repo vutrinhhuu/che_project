fd7c5cecf8190c7401030169fa110474f278df1b
vutrinhhuu <vutrinhhuu@gmail.com>
2019-11-02 11:49:41 +0700

    restrict access for admin only

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+    if resource.admin?
+      admin_dashboard_path
+    else
+      root_path
+    end
+  end
 end

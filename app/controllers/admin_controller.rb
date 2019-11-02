6cd1f0ee221c96cec241549b9bc505b162267338
vutrinhhuu <vutrinhhuu@gmail.com>
2019-11-02 11:49:41 +0700

    restrict access for admin only

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,12 @@
 class AdminController < ApplicationController
+  before_filter :verify_is_admin
   layout 'admin'
   def dashboard
+  end
+
+  private
+
+  def verify_is_admin
+    (current_user.nil?) ? redirect_to(root_path) : (redirect_to(root_path) unless current_user.admin?)
   end
 end

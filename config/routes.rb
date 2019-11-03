c25050dc71843da69d47c7f973f260d80c80ecc9
trungnn <trungnn160697@gmail.com>
2019-11-03 05:52:27 +0000

    chair detail

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   devise_for :users
   root "static_pages#home"
+  
+  # get list chairs
+  get "/chair", to:"chair#list_chair"
+  
+  #get chairs
+  resources :chairs
 end

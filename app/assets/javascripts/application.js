20d703193e35656069f59288394eb4a257278a04
trungnn <trungnn160697@gmail.com>
2019-11-04 03:13:04 +0000

    fix js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,14 +14,3 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-$(function(){
-  $("#show-limit").on("change", function(){
-    $(this).closest("form").trigger("submit");
-  });
-  $("#search").on("click", function(){
-    $(this).closest("form").trigger("submit");
-  });
-  $("#sort_by").on("change", function(){
-    $(this).closest("form").trigger("submit");
-  });
-})

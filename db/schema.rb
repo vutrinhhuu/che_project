0c99c31a3bff0fa3eaeacb10049910fe0046f353
EC2 Default User <ec2-user@ip-172-31-85-27.ec2.internal>
2019-11-20 03:57:15 +0000

    completed billings show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191113101222) do
+ActiveRecord::Schema.define(version: 20191120022729) do
+
+  create_table "billings", force: :cascade do |t|
+    t.decimal  "total",      precision: 12, scale: 3
+    t.string   "address"
+    t.integer  "user_id"
+    t.integer  "order_id"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["order_id"], name: "index_billings_on_order_id"
+    t.index ["user_id"], name: "index_billings_on_user_id"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"

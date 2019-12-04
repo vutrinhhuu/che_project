5f67a52b2b41995907dd82be5d12a4ba94354f3b
trungnn <trungnn160697@gmail.com>
2019-12-04 08:23:18 +0000

    fix slide page home

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
-    @slide_chairs = Chair.limit(3).where("quantity > 0").order(id: :desc)
+    @slide_chairs_office = Chair.limit(3).where("quantity > 0").where("category_id = 1").order(id: :desc)
+    @slide_chairs_wheel = Chair.limit(3).where("quantity > 0").where("category_id = 2").order(id: :desc)
     @list_chair = Chair.limit(8).order(id: :desc)
   end
 end

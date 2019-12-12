3595b41d67d067f7a4f565f4d77a6643d9637d13
vutrinhhuu <vutrinhhuu@gmail.com>
2019-12-12 15:59:19 +0700

    fix bug redirect when login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :store_user_location!, if: :storable_location?
   protect_from_forgery with: :exception
 
   helper_method :current_order
@@ -7,7 +8,7 @@ class ApplicationController < ActionController::Base
     if resource.admin?
       admin_dashboard_path
     else
-      root_path
+      stored_location_for(resource) || super
     end
   end
 
@@ -18,4 +19,19 @@ class ApplicationController < ActionController::Base
       Order.new
     end
   end
+
+  private
+    # Its important that the location is NOT stored if:
+    # - The request method is not GET (non idempotent)
+    # - The request is handled by a Devise controller such as Devise::SessionsController as that could cause an 
+    #    infinite redirect loop.
+    # - The request is an Ajax request as this can lead to very unexpected behaviour.
+    def storable_location?
+      request.get? && is_navigational_format? && !devise_controller? && !request.xhr? 
+    end
+
+    def store_user_location!
+      # :user is the scope we are authenticating
+      store_location_for(:user, request.fullpath)
+    end
 end
